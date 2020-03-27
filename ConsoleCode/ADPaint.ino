3e8aee8040fe0dd71b45a09b0f0cba3f39902034
DrSpyse <juliangaming2.0@gmail.com>
2020-03-27 16:03:13 -0400

    Worked on ADPaint

--- ConsoleCode/ADPaint.ino
+++ ConsoleCode/ADPaint.ino
@@ -1,7 +1,34 @@
 //All functions specific to ADPaint go here
 
+typedef struct{
+  int r;
+  int g;
+  int b;
+} rgbColor;
+
+//Sets initial colors - Change this to change available colors.
+rgbColor colors[10] = {
+    {255, 0, 0}, //RED
+    {0, 255, 0}, //GREEN
+    {0, 0, 255}, //BLUE
+    {255, 165, 0}, //ORANGE
+    {255, 255, 0}, //YELLOW
+    {128, 0, 128}, //PURPLE
+    {165, 42, 42}, //BROWN
+    {255, 192, 203}, //PINK
+    {255, 255, 255}, //WHITE
+    {0, 0, 0} //BLACK
+  };
+
+rgbColor currentColor;
+
+int ADPaint_brushSize;
+
 void launchADPaint(){
   bool playing = true;
+
+  //Reset variables
+  ADPaint_brushSize = 4;
   
   initADPaint();
   
@@ -13,10 +40,19 @@ void launchADPaint(){
 
 void initADPaint(){
   myGLCD.clrScr();
+
+  //Draw color options at bottom
+  for(int i = 0; i < 10; i++){
+    myGLCD.setColor(colors[i].r, colors[i].g, colors[i].b);
+    myGLCD.fillRect(i * 32 - 1, myGLCD.getDisplayYSize() - 21, (i + 1) * 32 -1, myGLCD.getDisplayYSize() - 1);
+  }
 }
 
 void updateADPaint(){
-  if(screenPressed){
-    //some stuff will probably happen
+  while(screenPressed){
+    readInputs();
+    currentColor = colors[5];
+    myGLCD.setColor(currentColor.r, currentColor.g, currentColor.b);
+    myGLCD.fillCircle(pressX, pressY, ADPaint_brushSize);
   }
 }
