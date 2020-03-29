36d103f6c480bfe2b64e593c59564dbc98c01dcd
DrSpyse <juliangaming2.0@gmail.com>
2020-03-28 23:00:31 -0400

    Worked on ADPaint some more
    Added color selection. Still need to add clear and fill functions, as well as changing brush size.

--- ConsoleCode/ADPaint.ino
+++ ConsoleCode/ADPaint.ino
@@ -44,15 +44,37 @@ void initADPaint(){
   //Draw color options at bottom
   for(int i = 0; i < 10; i++){
     myGLCD.setColor(colors[i].r, colors[i].g, colors[i].b);
-    myGLCD.fillRect(i * 32 - 1, myGLCD.getDisplayYSize() - 21, (i + 1) * 32 -1, myGLCD.getDisplayYSize() - 1);
+    myGLCD.fillRect(i * (myGLCD.getDisplayXSize() / 10) - 1, myGLCD.getDisplayYSize() - 21, (i + 1) * (myGLCD.getDisplayXSize() / 10) - 1, myGLCD.getDisplayYSize() - 1);
   }
+
+  //Draw divider above colors
+  myGLCD.setColor(255, 255, 255); //WHITE
+  myGLCD.drawLine(0, myGLCD.getDisplayYSize() - 31, myGLCD.getDisplayXSize() - 1, myGLCD.getDisplayYSize() - 31);
+
+  //Draw inital arrow
+  myGLCD.drawLine(0, myGLCD.getDisplayYSize() - 30, (myGLCD.getDisplayXSize() / 10 / 2), myGLCD.getDisplayYSize() - 22);
+  myGLCD.drawLine((myGLCD.getDisplayXSize() / 10) - 1, myGLCD.getDisplayYSize() - 30, (myGLCD.getDisplayXSize() / 10 / 2), myGLCD.getDisplayYSize() - 22);
+  
+  //Set starting color
+  currentColor = colors[0];
+  myGLCD.setColor(currentColor.r, currentColor.g, currentColor.b);
 }
 
 void updateADPaint(){
-  while(screenPressed){
-    readInputs();
-    currentColor = colors[5];
-    myGLCD.setColor(currentColor.r, currentColor.g, currentColor.b);
+  if(screenPressed){
+    if(pressY > myGLCD.getDisplayYSize() - 22){ //If a color at the bottom is pressed
+      //Clear indicator arrow
+      myGLCD.setColor(0, 0, 0); //BLACK
+      myGLCD.fillRect(0, myGLCD.getDisplayYSize() - 30, myGLCD.getDisplayXSize() - 1, myGLCD.getDisplayYSize() - 22);
+      //Draw new indicator arrow. Got this first try btw :)
+      myGLCD.setColor(255, 255, 255); //WHITE
+      myGLCD.drawLine((myGLCD.getDisplayXSize() / 10) * (pressX * 10 / myGLCD.getDisplayXSize()), myGLCD.getDisplayYSize() - 30, (myGLCD.getDisplayXSize() / 10) * (pressX * 10 / myGLCD.getDisplayXSize()) + (myGLCD.getDisplayXSize() / 10 / 2), myGLCD.getDisplayYSize() - 22);
+      myGLCD.drawLine((myGLCD.getDisplayXSize() / 10) * (pressX * 10 / myGLCD.getDisplayXSize()) + (myGLCD.getDisplayXSize() / 10) - 1, myGLCD.getDisplayYSize() - 30, (myGLCD.getDisplayXSize() / 10) * (pressX * 10 / myGLCD.getDisplayXSize()) + (myGLCD.getDisplayXSize() / 10 / 2), myGLCD.getDisplayYSize() - 22);
+      //Set new color
+      currentColor = colors[pressX * 10 / myGLCD.getDisplayXSize()]; //Makes number between 0 and 9 based on press
+      myGLCD.setColor(currentColor.r, currentColor.g, currentColor.b);
+    }
+    else if(pressY < myGLCD.getDisplayYSize() - 31 - ADPaint_brushSize)
     myGLCD.fillCircle(pressX, pressY, ADPaint_brushSize);
   }
 }
